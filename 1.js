4895458a57f7b8f73f965c58bf1f5d6d2c111681
Iryna <35580507+kovali@users.noreply.github.com>
2018-04-24 01:28:59 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,28 +1,9 @@
-function makeBuffer() {
-
-    var currentBuffer = [];
-
-    function bufferOps(value) {
-        return !value ? currentBuffer : currentBuffer.push(value);
-    }
-
-
-    bufferOps.clear = function () {
-        currentBuffer = [];
-    };
-
-    return bufferOps;
-}
-
-
-var buffer = makeBuffer();
- //make worker
+//make worker
 function Worker(worker) {
     this.name = worker.name;
     this.surname = worker.surname;
     this.rate = worker.rate;
     this.days = worker.days;
-    buffer(worker);
 }
 
 var worker1 = new Worker(
@@ -55,21 +36,65 @@ Worker.prototype.getSalary = function () {
 };
 
 
-Worker.prototype.getTotalSalary = function () {
-    var totalSalary = 0,
+function getTotalSalary(workers) {
+    var TotalSalary = 0;
 
-        workers = buffer();
-
-    for (var key in workers) {
-        totalSalary += workers[key].rate * workers[key].days;
+    for (var i = 0; i < workers.length; i++) {
+        var worker = workers[i];
+        var workerSalary = worker.getSalary();
+        TotalSalary = TotalSalary + workerSalary;
     }
 
-    return totalSalary;
+    return TotalSalary;
+}
 
-};
+
 console.log(worker1.name);
 console.log(worker1.surname);
 console.log(worker1.rate);
+console.log(worker1.days);
+
+console.log("Fist worker salary = " + worker1.getSalary());
+console.log("Second worker salary = " + worker2.getSalary());
+console.log("Third salary = " + worker3.getSalary());
+console.log("Total salary = " + getTotalSalary([worker1, worker2, worker3]));
+
+
+function Director() {
+}
+
+Director.prototype = Object.create(Worker.prototype);
+Director.prototype.constructor = Director;
+Director.prototype.workers = {};
+Director.prototype.addWorker = function (worker) {
+    this.workers[worker.name] = worker;
+};
+Director.prototype.deleteWorker = function (name) {
+    delete this.workers[name];
+};
+Director.prototype.getWorker = function (name) {
+    return this.workers[name];
+};
+Director.prototype.setWorkerRate = function (name, rate) {
+    this.getWorker(name).rate = rate;
+};
+
+var director = new Director({
+    name: 'Alec',
+    surname: 'P',
+    rate: 30,
+    days: 20
+});
+
+director.addWorker(worker1);
+director.addWorker(worker2);
+director.addWorker(worker3);
+
+console.log("Director workers: " + director.workers);
+
+console.log("First director worker salary: " + director.getWorker(worker1.name).getSalary());
+director.setWorkerRate(worker1.name, 900);
+console.log("First director worker salary after change: " + director.getWorker(worker1.name).getSalary());
 console.log(worker1.days);
 
 console.log(worker2.name);
